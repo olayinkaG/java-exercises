b0f3bdc903f65e953340838fe6d4f60e98cac292
Olayinka Afolabi <yinkore2003@yahoo.com>
2020-07-10 00:51:41 +0100

    Scanner declared

--- java excercises/src/com/chapt7/flightreservation/SeatReservation.java
+++ java excercises/src/com/chapt7/flightreservation/SeatReservation.java
@@ -1,9 +1,11 @@
 package com.chapt7.flightreservation;
 
+import java.util.Scanner;
+
 public class SeatReservation {
 
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
 	}
 
 }
